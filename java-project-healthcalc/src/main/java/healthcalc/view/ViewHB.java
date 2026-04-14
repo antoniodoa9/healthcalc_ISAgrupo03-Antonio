517861094f94f52073b63b15c3b0c3a0ad85fa2d
antoniodoa9 <antoniosanchezdona9@gmail.com>
2026-04-14 21:17:43 +0200

    feat: crear interfaz de la vista para Harris-Benedict (fixes #80)

---
+++ java-project-healthcalc/src/main/java/healthcalc/view/ViewHB.java
@@ -0,0 +1,24 @@
+package healthcalc.view;
+
+import java.awt.event.ActionListener;
+
+/**
+ * @author antoniodoa9
+ */
+public interface ViewHB {
+    
+    public double getWeight();
+
+    public double getHeight();
+
+    public int getAge();
+
+    public char getGender();  
+
+    public void setResult(double tmb);
+
+    public void setMessage(String msg); 
+
+    public void setController(ActionListener ctr);
+    
+}
