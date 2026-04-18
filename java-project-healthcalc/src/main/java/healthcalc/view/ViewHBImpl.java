d26c8c2f5446753167b4b758b7c5c154b3697a98
antoniodoa9 <antoniosanchezdona9@gmail.com>
2026-04-18 15:14:29 +0200

    style: hacer que las tres pestañas sean visualmente consistentes

--- java-project-healthcalc/src/main/java/healthcalc/view/ViewHBImpl.java
+++ java-project-healthcalc/src/main/java/healthcalc/view/ViewHBImpl.java
@@ -48,86 +48,87 @@ public class ViewHBImpl extends JPanel implements ViewHB {
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblTitulo, BorderLayout.NORTH);
 		
-		JPanel panelEnvoltorio = new JPanel();
-		panelEnvoltorio.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		add(panelEnvoltorio, BorderLayout.CENTER);
-				
-		JPanel panelContenedor = new JPanel();
-		panelContenedor.setLayout(new BorderLayout(0, 10));
-		panelEnvoltorio.add(panelContenedor);
-
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(4, 2, 10, 15));
-		panelContenedor.add(panel, BorderLayout.NORTH); 
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		add(panel, BorderLayout.CENTER);
+				
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(new BorderLayout(0, 10));
+		panel.add(panel_1);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(new GridLayout(4, 2, 10, 15));
+		panel_1.add(panel_2, BorderLayout.NORTH); 
 		
 		JLabel lblNewLabel_1 = new JLabel("Peso (kg):");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel_1);
+		panel_2.add(lblNewLabel_1);
 		
 		txtPeso = new JTextField();
-		panel.add(txtPeso);
+		panel_2.add(txtPeso);
 		txtPeso.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Altura (cm):");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel_2);
+		panel_2.add(lblNewLabel_2);
 		
 		txtAltura = new JTextField();
-		panel.add(txtAltura);
+		panel_2.add(txtAltura);
 		txtAltura.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Edad (años):");
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel_3);
+		panel_2.add(lblNewLabel_3);
 		
 		txtEdad = new JTextField();
-		panel.add(txtEdad);
+		panel_2.add(txtEdad);
 		txtEdad.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Género:");
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel_4);
+		panel_2.add(lblNewLabel_4);
 		
-		JPanel panel_1 = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		JPanel panel_3 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_3.getLayout();
 		flowLayout.setVgap(0);
 		flowLayout.setHgap(0);
-		panel.add(panel_1);
+		panel_2.add(panel_3);
 		
 		rbHombre = new JRadioButton("Hombre");
 		rbHombre.setSelected(true);
 		buttonGroup.add(rbHombre);
-		panel_1.add(rbHombre);
+		panel_3.add(rbHombre);
 		
 		JRadioButton rbMujer = new JRadioButton("Mujer");
 		buttonGroup.add(rbMujer);
-		panel_1.add(rbMujer);
+		panel_3.add(rbMujer);
 		
-		JPanel panel_AbajoFormulario = new JPanel();
-		panel_AbajoFormulario.setLayout(new BorderLayout(0, 5));
-		panelContenedor.add(panel_AbajoFormulario, BorderLayout.CENTER);
+		JPanel panel_4 = new JPanel();
+		panel_4.setLayout(new BorderLayout(0, 5));
+		panel_1.add(panel_4, BorderLayout.CENTER);
 		
 		lblMensajeError = new JLabel("");
 		lblMensajeError.setForeground(Color.RED);
 		lblMensajeError.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMensajeError.setBackground(Color.WHITE);
-		panel_AbajoFormulario.add(lblMensajeError, BorderLayout.NORTH);
+		panel_4.add(lblMensajeError, BorderLayout.NORTH);
 
-		JPanel panel_Resultado = new JPanel();
-		panel_AbajoFormulario.add(panel_Resultado, BorderLayout.CENTER);
-		panel_Resultado.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panel_5 = new JPanel();
+		panel_4.add(panel_5, BorderLayout.CENTER);
+		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		lblResultado = new JLabel("TMB: ---");
+		lblResultado.setBackground(new Color(255, 255, 255));
 		lblResultado.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblResultado.setBorder(new CompoundBorder(new LineBorder(new Color(64, 64, 64), 2), new EmptyBorder(5, 5, 5, 5)));
-		panel_Resultado.add(lblResultado);
+		panel_5.add(lblResultado);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		add(panel_2, BorderLayout.SOUTH);
+		JPanel panel_6 = new JPanel();
+		panel_6.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		add(panel_6, BorderLayout.SOUTH);
 		
 		btnCalcular = new JButton("Calcular");
-		panel_2.add(btnCalcular);
+		panel_6.add(btnCalcular);
 	}
 	@Override
 	public double getWeight() {
